a6eb4ce253280725cf127a6b687781c794086ddb
DaisakuHazui <daisakuhazui@Vino-MAC.local>
2018-09-24 18:14:21 +0900

    Add Export button in SeriesAdmin

--- django/mysite/polls/admin.py
+++ django/mysite/polls/admin.py
@@ -1,6 +1,21 @@
 from django.contrib import admin
+from django.conf.urls import url
+from django.http import HttpResponse
 
 from .models import Series, Card
 
-admin.site.register(Series)
+
+class SeriesAdmin(admin.ModelAdmin):
+    def get_urls(self):
+        urls = super().get_urls()
+        my_urls = [
+            url(r'^export/$', self.admin_site.admin_view(self.passcode_export), name='export'),
+        ]
+        return my_urls + urls
+
+    def passcode_export(self, request):
+        return HttpResponse('OK')
+
+
+admin.site.register(Series, SeriesAdmin)
 admin.site.register(Card)
