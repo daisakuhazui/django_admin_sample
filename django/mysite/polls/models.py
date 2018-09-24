ba88ada05a5bea79421f8c05be8fe0a641716a71
DaisakuHazui <daisakuhazui@Vino-MAC.local>
2018-09-24 17:31:51 +0900

    Add __str__ method in models.py

--- django/mysite/polls/models.py
+++ django/mysite/polls/models.py
@@ -5,7 +5,13 @@ class Series(models.Model):
     name = models.CharField(max_length=20)
     brires = models.TextField()
 
+    def __str__(self):
+        return u'%s' % self.name
+
 
 class Card(models.Model):
     series = models.ForeignKey(Series, on_delete=models.CASCADE)
     name = models.CharField(max_length=20)
+
+    def __str__(self):
+        return u'%s' % self.name
