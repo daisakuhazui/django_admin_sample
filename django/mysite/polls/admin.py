167f16ada7b781a646dbcf33ea4d82367e3bdff6
DaisakuHazui <daisakuhazui@Vino-MAC.local>
2018-09-24 20:41:10 +0900

    Add pk in export

--- django/mysite/polls/admin.py
+++ django/mysite/polls/admin.py
@@ -1,6 +1,7 @@
 from django.contrib import admin
 from django.conf.urls import url
 from django.http import HttpResponse
+from django.shortcuts import redirect
 
 from .models import Series, Card
 
@@ -9,11 +10,11 @@ class SeriesAdmin(admin.ModelAdmin):
     def get_urls(self):
         urls = super().get_urls()
         my_urls = [
-            url(r'^export/$', self.admin_site.admin_view(self.passcode_export), name='export'),
+            url(r'^(?P<pk>\d+)/export/$', self.admin_site.admin_view(self.passcode_export), name='export'),
         ]
         return my_urls + urls
 
-    def passcode_export(self, request):
+    def passcode_export(self, request, pk):
         return HttpResponse('OK')
 
 
